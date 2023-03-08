97ba672965ddba71bd91f6631fb9226df97a00b9
SanjeevM2004 <108799844+SanjeevM2004@users.noreply.github.com>
2023-03-09 01:58:24 +0530

    Update MSMEPage.js

--- src/components/MSMEPage.js
+++ src/components/MSMEPage.js
@@ -16,7 +16,8 @@ const MSME = () => {
                 <h1 className="msme-text">Welcome to the</h1>
                 <h2 className="msme-subtext"><b>MSME club</b></h2>
             </div>
-            <div class="d-grid gap-2 col-6 mx-auto">
+            <img className="msme-image" src="https://i.postimg.cc/V64xpjB8/MSME-3.jpg"></img>
+            <div class="d-grid col-4">
                 <Link to="/SignUp">
                     <button class="btn btn-primary-2" type="button"><b>Register</b></button>
                 </Link>
@@ -30,6 +31,34 @@ const MSME = () => {
                 <button class="btn btn-primary-2" type="button"><b>Loan & Transaction History</b></button>
                 </Link>
             </div>
+          <div className="footer-2">
+        <div className="page-foot-2" id="login">
+                            <div className="contact-info">
+                                <h1>CONTACT-INFO</h1> 
+                                <h3>
+                                    <ul>
+                                        <li>+91 9840120486</li>
+                                        <li>+91 8114790959</li>
+                                        <li>+91 9327686198</li>
+                                        <li>+91 8277609858</li>
+                                    </ul>
+                                </h3>  
+                                <div>
+                                    <p>
+                                        <a href="https://www.whatsapp.com/" className="contact-icons">
+                                            <i class="fa-brands fa-whatsapp" id="contact-icons"></i>
+                                        </a>
+                                        <a href="https://www.instagram.com/cfi_iitm_/" className="contact-icons">
+                                            <i class="fa-brands fa-instagram" id="contact-icons"></i>
+                                        </a>
+                                        <a href="https://twitter.com/" className="contact-icons">
+                                            <i class="fa-brands fa-twitter" id="contact-icons"></i>
+                                        </a>
+                                    </p>    
+                                </div>                     
+                            </div>
+                        </div>
+                        </div>
         </html>
         </>
     )
